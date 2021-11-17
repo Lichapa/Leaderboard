8fdd1c97967d6bff23d87b855ddd75b3b1d43be1
Lichapa <lichapau@gmail.com>
2021-11-17 18:53:49 +0200

    Removed the Array object

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 import './style.css';
 
-const scoresList = [{
-  player: 'Name',
-  score: 100,
-},
-{
-  player: 'Mary',
-  score: 80,
-},
-{
-  player: 'Marion',
-  score: 90,
-},
-];
-
 const board = (list) => {
   const myList = document.getElementById('scoreList');
   const listElement = document.createElement('li');
